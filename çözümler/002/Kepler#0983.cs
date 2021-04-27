c346d9de27a94ceea22a32a232c77897e95a3f8c
OwnSociety <75059946+OwnSociety@users.noreply.github.com>
2021-04-27 12:00:59 +0300

    Add files via upload

---
+++ çözümler/002/Kepler#0983.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Yazı_tura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndm = new Random();
+            string brah = "";
+            string[] kelimeler = { "yazı", "tura" };
+            for(int i =1; i<=2; i++)
+            {
+                brah = kelimeler[rndm.Next(0, 2)].ToString();  
+            }
+
+            Console.WriteLine(brah);
+            }
+           
+        }
+    }
