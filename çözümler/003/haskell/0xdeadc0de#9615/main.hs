cb921f6f288cee215e515632081509fc73849c04
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-04-27 08:20:33 +0000

    003 final 0xdeadc0de

--- çözümler/003/haskell/0xdeadc0de#9615/main.hs
+++ çözümler/003/haskell/0xdeadc0de#9615/main.hs
@@ -1,2 +1,10 @@
+import Text.Read (readMaybe)
+
 main = do
-    yas <- readMaybe 
+    putStrLn "Saniye cinsinden yaşınızı hesaplayalım. Yaşınız kaç: "
+    yaş <- getLine >>= return . readMaybe :: IO (Maybe Int)
+    -- yaş atamasinda IO monad yokolup Maybe monad kalacak
+
+    putStrLn $ "Saniye yaşınız: " <> maybe b ab yaş where
+        ab = show . (*(365*24*60*60))
+        b = "<geçersiz giriş tekrar deneyiniz>"
